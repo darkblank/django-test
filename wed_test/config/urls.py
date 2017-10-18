adbe23d7a35ac099e527bcab5955e0a4bf1811b0
darkblank <darkblank1990@gmail.com>
2017-10-19 02:41:58 +0900

    post_detail 기능 추가

--- wed_test/config/urls.py
+++ wed_test/config/urls.py
@@ -18,11 +18,12 @@ from django.conf.urls import url
 from django.conf.urls.static import static
 from django.contrib import admin
 
-from post.views import post_list
+from post.views import post_list, post_detail
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^post/$', post_list, name='post_list'),
+    url(r'^post/(?P<post_pk>\d+)$', post_detail, name='post_detail'),
 ]
 
 urlpatterns += static(
